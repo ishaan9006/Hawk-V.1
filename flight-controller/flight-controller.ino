ffecf25f94815ef6db83e92b2f7213a081b9a91d
Ishaan Khullar <ishaan.khullar06@gmail.com>
2022-12-18 20:36:24 +0530

    Defined pins!

--- flight-controller/flight-controller.ino
+++ flight-controller/flight-controller.ino
@@ -1,9 +1,38 @@
+
+#include <Wire.h>
+
+// Pins for reciever channels
+#define CHANNEL 0
+#define CHANNEL 1
+#define CHANNEL 2
+#define CHANNEL 3
+
+#define X 0
+#define Y 1
+#define Z 2
+
+
+// Motor pins
+const int motor1 = 5;
+const int motor2 = 6;
+const int motor3 = 10;
+const int motor4 = 11;
+
+// LED
+const int led = 13;
+
 void setup() {
-  // put your setup code here, to run once:
+  Wire.begin();
+ 
+  pinMode(led, OUTPUT);
+  
+  pinMode(motor1, OUTPUT); 
+  pinMode(motor2, OUTPUT); 
+  pinMode(motor3, OUTPUT); 
+  pinMode(motor4, OUTPUT);  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
