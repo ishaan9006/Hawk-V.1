6b8dc48bca02b47a164aa39404f970e69c11eeac
Ishaan Khullar <ishaan.khullar06@gmail.com>
2022-12-19 21:09:27 +0530

    Added pid variables!

--- flight-controller/flight-controller.ino
+++ flight-controller/flight-controller.ino
@@ -7,10 +7,24 @@
 #define CHANNEL 2
 #define CHANNEL 3
 
+// MPU Address
+#define MPU_ADDRESS 0x68
+
+
 #define X 0
 #define Y 1
 #define Z 2
 
+
+// LED
+const int led = 13;
+
+// MPU DATA
+int gyro_raw[3] = {0, 0, 0};           //Data in deg/sec. (X, Y, Z)
+long gyro_offset[3] = {0, 0, 0};       //Avg offset of each X,Y,Z
+
+int acc_raw[3] = {0, 0, 0};            //Data in m/sec^2. (X, Y, X)
+float acc_angle[3] = {0, 0, 0};        //Calculated angles of each X,Y,X
 
 // Motor pins
 const int motor1 = 5;
@@ -18,21 +32,33 @@ const int motor2 = 6;
 const int motor3 = 10;
 const int motor4 = 11;
 
-// LED
-const int led = 13;
+// PID Control variables
+
+float Kp[3] = {0, 0, 0};
+float Ki[3] = {0, 0, 0};
+float Kd[3] = {0, 0, 0};
 
 void setup() {
   Wire.begin();
  
   pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+
+  calibrateGyro();
   
   pinMode(motor1, OUTPUT); 
   pinMode(motor2, OUTPUT); 
   pinMode(motor3, OUTPUT); 
   pinMode(motor4, OUTPUT);  
 
+  digitalWrite(led, LOW);
+
 }
 
 void loop() {
 
+}
+
+void calibrateGyro(){
+  
 }
